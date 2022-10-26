e60bc79d97b705519b863924b84fe5bd7a48f143
Alejandro Pascual <apascual@twilio.com>
2022-10-26 13:20:04 +0200

    style buttons

--- src/lib/AboutMe.svelte
+++ src/lib/AboutMe.svelte
@@ -3,7 +3,7 @@
 
 <div class="about-me">
   <div class="card main">
-    <img class="portrait" src="./assets/portrait.png" alt="Portrait"/>
+    <img src="./assets/portrait.png" alt="Portrait" class="portrait"/>
     <div>
       <p class="presentation">
         Hey! I'm Alejandro and I love using modern tools to build reliable and efficient solutions to challenging problems.
@@ -12,15 +12,15 @@
     </div>
   </div>
   <div class="links">
-    <div class="card link blog">
-    Check out my blog
-    </div>
-    <div class="card link github">
+    <a href="." class="card link blog">
+      Check out my blog
+    </a>
+    <a href="https:/www.github.com/pascualex" class="card link github">
       GitHub
-    </div>
-    <div class="card link twitter">
+    </a>
+    <a href="https://twitter.com/Pascualex22" class="card link twitter">
       Twitter
-    </div>
+    </a>
   </div>
 </div>
 
@@ -33,7 +33,7 @@
   }
 
   .card {
-    background-color: rgb(240, 240, 240);
+    background-color: #f0f0f0;
     border-radius: 24px;
     justify-content: center;
     align-items: center;
@@ -61,16 +61,29 @@
     padding: 16px;
     display: flex;
   }
+  .link:hover {
+    font-weight: bold;
+    color: #f0f0f0;
+  }
 
   .blog {
     flex: 3;
+  }
+  .blog:hover {
+    background-color: #00B397;
   }
 
   .github {
     flex: 1;
   }
+  .github:hover {
+    background-color: #F47351;
+  }
 
   .twitter {
     flex: 1;
+  }
+  .twitter:hover {
+    background-color: #1da1f2;
   }
 </style>
